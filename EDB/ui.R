9e377dd9cd1f32d57e87e13832af97c0182f52d9
Faith Kemunto <57103980+fayeee-e@users.noreply.github.com>
2022-08-31 12:14:38 +0300

    Update ui.R

--- EDB/ui.R
+++ EDB/ui.R
@@ -2,7 +2,7 @@ library(shiny)
 library(ggplot2)
 library(DBI)
 library(RMySQL)
-con = dbConnect(MySQL(), user='sesom_2015', password='sesom_2015', db='faith', host='mtekib.cyil3u7sobwf.us-west-2.rds.amazonaws.com')
+con = dbConnect(MySQL(), user='', password='', db='faith', host='mtekib.cyil3u7sobwf.us-west-2.rds.amazonaws.com')
 query1 = 'SELECT DISTINCT county from indicators4'
 query= 'SELECT county, indicator, mark from indicators4'
 result1 = dbGetQuery(con, query1)
